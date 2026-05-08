f5848a66a18fda37c0e825c933547d87bb7f6af6
keziazahiljohn <keziazahiljohn@gmail.com>
2026-05-08 12:44:42 -0400

    created dealership class and helper methods Phase 1 complete

--- src/main/java/com/pluralsight/Dealership.java
+++ src/main/java/com/pluralsight/Dealership.java
@@ -1,4 +1,80 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+
+        inventory = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Vehicle> getVehiclesByPrice(double min, double max) {
+        return null; // empty method -- not complete
+    }
+
+    public List<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null; // empty method -- not complete
+    }
+
+    public List<Vehicle> getVehiclesByYear(int min, int max) {
+        return null; // empty method -- not complete
+    }
+
+    public List<Vehicle> getVehiclesByColor(String color) {
+        return null; // empty method -- not complete
+    }
+
+    public List<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null; // empty method -- not complete
+    }
+
+    public List<Vehicle> getVehiclesByType(String vehicleType) {
+        return null; // empty method -- not complete
+    }
+
+    public List<Vehicle> getAllVehicles() {
+        return null; // empty method -- not complete
+    }
+
+    public Vehicle addVehicle(Vehicle vehicle) {
+        return null; // empty method -- not complete
+    }
+
+    public void removeVehicle(Vehicle vehicle) {
+
+    }
 }
